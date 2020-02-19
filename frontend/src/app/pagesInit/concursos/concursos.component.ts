4c5903893981b55a31946b2750ff5d8c30ea4287
Eduard <eduard.duarte@hotmail.com>
2020-02-19 10:02:15 -0500

    order comentarios

--- frontend/src/app/pagesInit/concursos/concursos.component.ts
+++ frontend/src/app/pagesInit/concursos/concursos.component.ts
@@ -40,11 +40,10 @@ export class ConcursosComponent implements OnInit {
   youtubeUrl = "https://www.youtube.com/watch?v=gVsEm_QRWiQ&list=RDgVsEm_QRWiQ&start_radio=1";
 
   //Datos child jw-playerId
-  files = [
-    { 'file': "http://172.24.42.42:8080/resources/upload/No%20me%20arrepiento.mp4" },
-    { 'file': "http://172.24.42.42:8080/resources/upload/No%20me%20arrepiento.mp4" },
-    { 'file': "http://172.24.42.42:8080/resources/upload/No%20me%20arrepiento.mp4" },
-  ];
+  // files = [
+  //   { 'file': "http://172.24.42.42:8080/resources/upload/No%20me%20arrepiento.mp4" },
+  //   { 'file': "http://172.24.42.42:8080/resources/upload/No%20me%20arrepiento.mp4" },
+  // ];
   file: string = 'http://172.24.42.42:8080/resources/upload/No%20me%20arrepiento.mp4';
   height: string = "420px";
   width: string = "680px";
@@ -144,33 +143,6 @@ export class ConcursosComponent implements OnInit {
     this._document.defaultView.location.reload();
   }
 
-
-  private serverList: any = {
-    "cdnServers": [
-      {
-        "title": "shrek-4",
-
-        "sources": [
-          // {
-          //   "file": "https://media6.comwel.net/videos/GQlE6Rqd-AgylS15n.mp4",
-          //   "label": "240"
-          // },
-          // {
-          //   "file": "https://media6.comwel.net/videos/GQlE6Rqd-8LSW5F2t.mp4",
-          //   "label": "360"
-          // },
-          // {
-          //   "file": "https://media6.comwel.net/videos/GQlE6Rqd-PA9rXzRh.mp4",
-          //   "label": "480"
-          // }
-
-          { "title": "Driving USA", "description": "Beautiful sunset point of view shot along empty desert highway through Monument Valley, Arizona Utah", "kind": "Single Item", "playlist": [{ "title": "Driving USA", "mediaid": "8L4m9FJB", "link": "https://cdn.jwplayer.com/previews/8L4m9FJB", "image": "https://cdn.jwplayer.com/v2/media/8L4m9FJB/poster.jpg?width=720", "images": [{ "src": "https://cdn.jwplayer.com/v2/media/8L4m9FJB/poster.jpg?width=320", "width": 320, "type": "image/jpeg" }, { "src": "https://cdn.jwplayer.com/v2/media/8L4m9FJB/poster.jpg?width=480", "width": 480, "type": "image/jpeg" }, { "src": "https://cdn.jwplayer.com/v2/media/8L4m9FJB/poster.jpg?width=640", "width": 640, "type": "image/jpeg" }, { "src": "https://cdn.jwplayer.com/v2/media/8L4m9FJB/poster.jpg?width=720", "width": 720, "type": "image/jpeg" }], "duration": 30, "pubdate": 1495054284, "description": "Beautiful sunset point of view shot along empty desert highway through Monument Valley, Arizona Utah", "tags": "stock video", "sources": [{ "file": "https://cdn.jwplayer.com/manifests/8L4m9FJB.mpd", "type": "application/dash+xml", "mediaTypes": ["video/webm; codecs=\"vp9\""] }, { "file": "https://cdn.jwplayer.com/manifests/8L4m9FJB.m3u8", "type": "application/vnd.apple.mpegurl" }, { "file": "https://cdn.jwplayer.com/videos/8L4m9FJB-Zq6530MP.mp4", "type": "video/mp4", "height": 180, "width": 320, "label": "H.264 320px" }, { "file": "https://cdn.jwplayer.com/videos/8L4m9FJB-TNpruJId.mp4", "type": "video/mp4", "height": 270, "width": 480, "label": "H.264 480px" }, { "file": "https://cdn.jwplayer.com/videos/8L4m9FJB-cIp6U8lV.mp4", "type": "video/mp4", "height": 406, "width": 720, "label": "H.264 720px" }, { "file": "https://cdn.jwplayer.com/videos/8L4m9FJB-FctPAkow.mp4", "type": "video/mp4", "height": 720, "width": 1280, "label": "H.264 1280px" }, { "file": "https://cdn.jwplayer.com/videos/8L4m9FJB-8yQ1cYbs.mp4", "type": "video/mp4", "height": 1080, "width": 1920, "label": "H.264 1920px" }], "tracks": [{ "file": "https://cdn.jwplayer.com/strips/8L4m9FJB-120.vtt", "kind": "thumbnails" }], "variations": {} }], "feed_instance_id": "a00fc959-d0c2-4c4c-b520-cee2d78e8c2d" }
-
-        ]
-      },
-
-    ]
-  };
 
   ngAfterViewInit() {
     // console.log('file: ', this.file);
@@ -306,3 +278,31 @@ export class ChargeVDialogComponent {
   }
 
 }
+
+
+// private serverList: any = {
+  //   "cdnServers": [
+    //     {
+      //       "title": "shrek-4",
+      //
+      //       "sources": [
+        //         // {
+          //         //   "file": "https://media6.comwel.net/videos/GQlE6Rqd-AgylS15n.mp4",
+          //         //   "label": "240"
+          //         // },
+          //         // {
+            //         //   "file": "https://media6.comwel.net/videos/GQlE6Rqd-8LSW5F2t.mp4",
+            //         //   "label": "360"
+            //         // },
+            //         // {
+              //         //   "file": "https://media6.comwel.net/videos/GQlE6Rqd-PA9rXzRh.mp4",
+              //         //   "label": "480"
+              //         // }
+              //
+              //         { "title": "Driving USA", "description": "Beautiful sunset point of view shot along empty desert highway through Monument Valley, Arizona Utah", "kind": "Single Item", "playlist": [{ "title": "Driving USA", "mediaid": "8L4m9FJB", "link": "https://cdn.jwplayer.com/previews/8L4m9FJB", "image": "https://cdn.jwplayer.com/v2/media/8L4m9FJB/poster.jpg?width=720", "images": [{ "src": "https://cdn.jwplayer.com/v2/media/8L4m9FJB/poster.jpg?width=320", "width": 320, "type": "image/jpeg" }, { "src": "https://cdn.jwplayer.com/v2/media/8L4m9FJB/poster.jpg?width=480", "width": 480, "type": "image/jpeg" }, { "src": "https://cdn.jwplayer.com/v2/media/8L4m9FJB/poster.jpg?width=640", "width": 640, "type": "image/jpeg" }, { "src": "https://cdn.jwplayer.com/v2/media/8L4m9FJB/poster.jpg?width=720", "width": 720, "type": "image/jpeg" }], "duration": 30, "pubdate": 1495054284, "description": "Beautiful sunset point of view shot along empty desert highway through Monument Valley, Arizona Utah", "tags": "stock video", "sources": [{ "file": "https://cdn.jwplayer.com/manifests/8L4m9FJB.mpd", "type": "application/dash+xml", "mediaTypes": ["video/webm; codecs=\"vp9\""] }, { "file": "https://cdn.jwplayer.com/manifests/8L4m9FJB.m3u8", "type": "application/vnd.apple.mpegurl" }, { "file": "https://cdn.jwplayer.com/videos/8L4m9FJB-Zq6530MP.mp4", "type": "video/mp4", "height": 180, "width": 320, "label": "H.264 320px" }, { "file": "https://cdn.jwplayer.com/videos/8L4m9FJB-TNpruJId.mp4", "type": "video/mp4", "height": 270, "width": 480, "label": "H.264 480px" }, { "file": "https://cdn.jwplayer.com/videos/8L4m9FJB-cIp6U8lV.mp4", "type": "video/mp4", "height": 406, "width": 720, "label": "H.264 720px" }, { "file": "https://cdn.jwplayer.com/videos/8L4m9FJB-FctPAkow.mp4", "type": "video/mp4", "height": 720, "width": 1280, "label": "H.264 1280px" }, { "file": "https://cdn.jwplayer.com/videos/8L4m9FJB-8yQ1cYbs.mp4", "type": "video/mp4", "height": 1080, "width": 1920, "label": "H.264 1920px" }], "tracks": [{ "file": "https://cdn.jwplayer.com/strips/8L4m9FJB-120.vtt", "kind": "thumbnails" }], "variations": {} }], "feed_instance_id": "a00fc959-d0c2-4c4c-b520-cee2d78e8c2d" }
+              //
+              //       ]
+              //     },
+              //
+              //   ]
+              // };
