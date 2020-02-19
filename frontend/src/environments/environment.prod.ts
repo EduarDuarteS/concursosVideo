153eea421d7e11205f3d9fe9f8894db8c49f8c14
Eduard <eduard.duarte@hotmail.com>
2020-02-19 18:34:34 -0500

    nuevo ambiente

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://172.24.42.42:8080'
+  apiUrl: 'http://172.24.42.61:8082'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,10 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://172.24.42.42:8080'
+  // apiUrl: 'http://172.24.42.42:8080'
+  apiUrl: 'http://172.24.42.61:8082'
+
+
 
 
 };
