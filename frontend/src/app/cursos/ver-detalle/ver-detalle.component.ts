a8a418cac2a1d60fcc2edb06be4e7ac95f2ba1ff
Eduard <eduard.duarte@hotmail.com>
2020-02-21 17:56:39 -0500

    carga datos concurso

--- frontend/src/app/cursos/ver-detalle/ver-detalle.component.ts
+++ frontend/src/app/cursos/ver-detalle/ver-detalle.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Inject, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+import { URLUnicaService } from "../../services/urlUnica.service";
+import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+
+
+declare var jwplayer: any;
+
 
 @Component({
   selector: 'app-ver-detalle',
@@ -7,9 +14,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerDetalleComponent implements OnInit {
 
-  constructor() { }
+  href;
+  urlUnica;
+  concurso;
+
+  //paginator
+  displayedColumns = ['Videos'];
+  // dataSource: any = new MatTableDataSource<any>();
+  dataSource = new MatTableDataSource([]);
+  sort;
+  paginator;
+  applyFilter;
+
+  // @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: false }) set matSort(ms: MatSort) {
+    this.sort = ms;
+    this.setDataSourceAttributes();
+  }
+
+  @ViewChild(MatPaginator, { static: false }) set matPaginator(mp: MatPaginator) {
+    this.paginator = mp;
+    this.setDataSourceAttributes();
+  }
+
+  setDataSourceAttributes() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+
+    if (this.paginator && this.sort) {
+      this.applyFilter('');
+    }
+  }
+
+  constructor(private router: Router,
+    private urlUnicaService: URLUnicaService,
+  ) { }
 
   ngOnInit() {
+    this.href = this.router.url;
+    console.log(this.router.url);
+    console.log(this.href.split('/').pop());
+    this.urlUnica = '/'+this.href.split('/').pop();
+
+        this.urlUnicaService.getConcurso(this.urlUnica)
+          .subscribe(
+            result => {
+              this.concurso = result;
+              console.log(result);
+
+              if (!result) {
+              }
+            },
+            error => {
+              console.log(error);
+            },
+            () => {
+              // this.router.navigate(['/admin/eventos']);
+            }
+          );
   }
 
 }
